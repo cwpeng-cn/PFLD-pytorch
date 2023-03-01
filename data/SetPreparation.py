6b34207fb5192cf5acd8b772f36a36641ab81f6f
cwpeng-cn <cw.peng@foxmail.com>
2023-03-01 22:27:50 +0800

    fix: fix path bug

--- data/SetPreparation.py
+++ data/SetPreparation.py
@@ -198,7 +198,7 @@ def get_dataset_list(imgDir, outDir, landmarkDir, is_train):
 if __name__ == '__main__':
     root_dir = os.path.dirname(os.path.realpath(__file__))
     imageDirs = 'WFLW/WFLW_images'
-    Mirror_file = 'WFLW/WFLW_annotations/Mirror98.txt'
+    Mirror_file = 'Mirror98.txt'
 
     landmarkDirs = ['WFLW/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_test.txt',
                     'WFLW/WFLW_annotations/list_98pt_rect_attr_train_test/list_98pt_rect_attr_train.txt']
